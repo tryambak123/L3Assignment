3cf22304cef8f50e36bee4cc52e3c88069415a2e
Tryambak Nath jha <tr20462725@wipro.com>
2025-07-19 00:11:35 +0530

    TS

--- assignment4/add.php
+++ assignment4/add.php
@@ -52,7 +52,7 @@ if(isset($_GET['msg'])){
 		<tr><td>Sub5</td><td><input type="number" name="sub5" min="0" max="100" onkeyup="enforceMinMax(this)" onfocusout="calculateTotal(this)" required></td></tr>
 		<tr><td>Total</td><td><input type="number" name="total" min="0" max="500" id="marks_sum" onkeyup="enforceMinMax(this)" required></td></tr>
 		<tr><td>Total Marks</td><td><input type="number" name="total_marks" value = "500" readonly></td></tr>
-		<tr><td  colspan="5"><input type="submit" name="submit" value="submit"></td></tr>
+		<tr><td  colspan="2"><input type="submit" name="submit" value="submit"></td></tr>
 	</table>
 </form>
 </body>
@@ -71,8 +71,8 @@ if(isset($_POST['submit'])){
 	$total_marks = trim($_POST['total_marks']);
 		
 	if($error != null){
-		header('Location: ' . $_SESSION['PHP_SELF'].'?error='.$error);
-       	die();
+		//header('Location: ' . $_SESSION['PHP_SELF'].'?error='.$error);
+       	die('error');
 	}else{		
 		$statement = $connect->prepare("INSERT INTO class1(name,sub1,sub2,sub3,sub4,sub5,total,total_marks) values(?,?,?,?,?,?,?,?,?,?)");
 		$statement->execute([$name, $sub1, $sub2, $sub3,$sub4, $sub5,$total,$total_marks]);
